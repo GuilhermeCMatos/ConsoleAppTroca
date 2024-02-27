dfa75943d4218dfdab73614d1273e8e0bb562dcf
Usuario <Usuario@CPD03-M07>
2024-02-27 11:58:02 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppTroca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTroca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fale o valor inteiro A: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fale o valor inteiro B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int c = b;
+
+            int b = a;
+
+            int a = c;
+
+            Console.WriteLine("Valor de A: " + A + " Valor de B:" + B);
+
+            Console.ReadKey();
+        }
+    }
+}
